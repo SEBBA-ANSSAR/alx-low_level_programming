8c862ea6efbd04e7ec85476ed3f815ecf2c9c710
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-09 12:56:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main: entry point to print the string in the put function
- * discription:this program prints Programming is like building a multilingual puzzle
+ * discription: by using main function 
+ * this program prints 'Programming is like building a multilingual puzzle
  * Return : 0
  */
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
