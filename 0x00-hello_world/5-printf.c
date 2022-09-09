f3402e352eaea65880c3614793eddc6f73455e41
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-09 13:38:53 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
 * main - print the string by using printf function
 *Discription: by using this prohram
-* this program prints "with proper grammar, but the outcome is a piece of art
-* return: 0
+* this program prints "with proper grammar, but the outcome is a piece of art,
+* Return: 0
 */
 int main(void)
 {
