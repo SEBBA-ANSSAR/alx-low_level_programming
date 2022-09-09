33e1d9190e17a5e8dadaab639337e89aac3dcb00
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-09 14:29:51 -0700

    quots

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
+
+
 /**
+
  * main - Entry point
+
  *
- * Return: 1
+
+ * Return: Always 0 (Success)
+
  */
+
 int main(void)
+
 {
 
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+
+
 	return (1);
 
 }
