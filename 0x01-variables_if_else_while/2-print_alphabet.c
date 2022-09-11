83ba7dcbf4f9ea7e5472718139d316bdf16db81d
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-11 03:09:48 -0700

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the alphabet in lowercase, followed by a new line.
+ * main - a program that prints the alphabet in lowercase,
+ * followed by a new line.
  * Description: You can only use the putchar function
  * (every other function (printf, puts, etc…) is forbidden)
  *  All your code should be in the main function
