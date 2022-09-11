b91b6ec0c4bc62ffd81a3c753aaa9df73f4c4f25
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-11 02:49:09 -0700

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,12 @@
  *             The string Last digit of, followed by
  *             n, followed by
  *             the string is, followed by
- *                       if the last digit of n is greater than 5: the string and is greater than 5
- *                       if the last digit of n is 0: the string and is 0
- *                       if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+ *             if the last digit of n is greater than 5:
+ *             the string and is greater than 5
+ *             if the last digit of n is 0: the string and is 0
+ *             if the last digit of n is less than 6 and not 0:
+ *             the string and is less than 6 and not 0
+ * Return: 0
 */
 int main(void)
 {
