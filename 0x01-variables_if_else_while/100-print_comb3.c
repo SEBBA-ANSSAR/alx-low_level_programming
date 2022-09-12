bbad4bbae3644e7942d5f872ffd6b64f5128c9ee
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-12 11:59:54 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,21 +19,24 @@
 */
 int main(void)
 {
-	int d;
+	int d1,d2;
 
-	for (d = 0; d < 90; d++)
+	for (d1 = 0; d1 < 9; d1++)
 	{
-		for ()
+		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
+			putchar((d1 % 10), '0');
+			putchar((d2 % 10), '0');
+
+			for (d1 == 8, && d2 == 9)
+				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
-	}	
+	}
+
 	putchar('\n');
+
 	return (0);
 }
