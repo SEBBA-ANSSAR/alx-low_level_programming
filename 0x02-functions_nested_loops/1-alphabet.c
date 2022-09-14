5c3ddb829e36dbd40c0dab9ab9d9392e2c381947
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-14 05:24:58 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet, in lowercase
+ * followed by new line
+ * Description:
+ * Prototype: void print_alphabet(void);
+ * we can only use _putchar twice in your code
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	int s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
