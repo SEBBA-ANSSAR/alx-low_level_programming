1862ca432df818859f44f6a07a5df92a457cf10e
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-14 22:08:36 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,8 +4,8 @@
  *  _isalpha - a function that checks for alphabetic character.
  *  Description:Prototype int _isalpha(int s)
  *  @s: single letter input
- *  Returns 1 if s is a letter, lowercase or uppercase
- *  Return: 0 
+ *  Return: 1 if s is a letter, lowercase or uppercase
+ *  Return: 0
 */
 int _isalpha(int s)
 {
