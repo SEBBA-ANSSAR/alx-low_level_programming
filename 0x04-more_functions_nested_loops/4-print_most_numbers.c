9ad322bed45efca545948da00596d55628aaa89b
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-15 13:32:12 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * followed by newline
+ * Return: 0
+*/
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50)
+		{
+			if (a != 52)
+			{
+				_putchar(a);
+			}
+		}
+	}
+	_putchar('\n');
+}
