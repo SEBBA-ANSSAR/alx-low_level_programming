0b9d83fadb58d42729840b9fc9121255a3d256d0
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-15 04:28:26 -0700

    0-main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void posetive_or_negative(int i);
+void print_remaning_days(int, int, int);
+int largest_number(int, int, int);
+int conver_day(int, int);
+
+#endif
