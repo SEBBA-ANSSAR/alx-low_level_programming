d680e1ad2ea8589e2558b714c831d83f2a4bae66
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-15 13:51:53 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,8 @@
 /*
  * more_numbers - a function that prints 10 times the numbers, from 0 to 14
  * followed by new line
- * Return:0
+ * Description: prototype void more_numbers(void);
+ * Return: 0
 */
 void more_numbers(void)
 {
