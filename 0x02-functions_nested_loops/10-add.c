e59384dca7d26345eb12b8daeb2bbeed300c695c
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-15 00:47:05 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 /**
  *  add - a function that adds two integers and returns the result
  *  Description: Prototype int add(int, int);
- *  a: first number input
- *  b: second number input
+ *  @a: first number input
+ *  @b: second number input
  *  Return: one + two
 */
 int add(int a, int b)
