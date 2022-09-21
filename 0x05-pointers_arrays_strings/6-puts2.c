98f858aed19d292643bc8c98f855738cc304c235
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-21 13:47:17 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,13 @@
 */
 void puts2(char *str)
 {
-	int index = 0;
-	int len = 0;
+	int i;
 
-	while (str[index++])
-		len++;
-	for (index = 0; index < len; index +=2)
-		_putchar(str[index]);
-
-	_putchar('\n)';
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
