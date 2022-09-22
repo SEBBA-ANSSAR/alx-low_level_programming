8a9ed344f394ce9482e32b9bc0a86d29386458e9
SEBBA-ANSSAR <sebhadinanssar@gmail.com>
2022-09-22 10:56:41 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,17 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
+	int len = 0, i;
 
-	while (dest[dlen])
+	while (dest[len])
+		len++;
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dlen++;
+		dest[len] = src[i];
+		len++;
 	}
 
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-
-	dest[dlen] = '\0';
+	dest[len] = '\0';
 	return (dest);
 }
